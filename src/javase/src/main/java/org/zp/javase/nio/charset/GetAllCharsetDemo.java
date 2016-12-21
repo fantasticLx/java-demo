1bc22653b820c2559228d0c51cd7ca4553512508
victor zhang <atalantis1024@163.com>
2016-12-21 21:53:17 +0800

    Java NIO 范例

---
+++ src/javase/src/main/java/org/zp/javase/nio/charset/GetAllCharsetDemo.java
@@ -0,0 +1,20 @@
+package org.zp.javase.nio.charset;
+
+import java.nio.charset.Charset;
+import java.util.SortedMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 取得Charset类的全部编码
+ */
+public class GetAllCharsetDemo{
+	public static void main(String args[]) {
+		SortedMap<String, Charset> all = Charset.availableCharsets(); // 得到全部可用的字符集
+		Iterator<Map.Entry<String, Charset>> iter = all.entrySet().iterator();
+		while(iter.hasNext()) {
+			Map.Entry<String,Charset> me = iter.next();
+			System.out.println(me.getKey() + " --> " + me.getValue());
+		}
+	}
+}
