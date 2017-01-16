ab384841750338193031624d84be5558b1d7b848
victor zhang <atalantis1024@163.com>
2017-01-16 17:43:08 +0800

    添加读取文件字节内容的功能

--- javaee/tools/src/main/java/org/zp/javaee/tools/file/FileUtil.java
+++ javaee/tools/src/main/java/org/zp/javaee/tools/file/FileUtil.java
@@ -5,6 +5,10 @@
 package org.zp.javaee.tools.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * @author victor zhang
@@ -15,16 +19,45 @@ public class FileUtil {
         return System.getProperty("user.dir");
     }
 
-    public static String getCurrentPath(){
+    public static String getCurrentPath() {
         String classpath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         return classpath.substring(1, classpath.indexOf("/target/classes/"));
     }
 
-    public static String getResourcesPath(){
+    public static String getResourcesPath() {
         String classpath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         return classpath.substring(1, classpath.indexOf("/target")) + "/src/main/resources";
     }
 
+    /**
+     * 读取文件字节内容，可以在处理大文件时，提升性能
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray(String filename) throws IOException {
+        FileChannel fc = null;
+        try {
+            fc = new RandomAccessFile(filename, "r").getChannel();
+            MappedByteBuffer byteBuffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size()).load();
+            System.out.println(byteBuffer.isLoaded());
+            byte[] result = new byte[(int) fc.size()];
+            if (byteBuffer.remaining() > 0) {
+                byteBuffer.get(result, 0, byteBuffer.remaining());
+            }
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                fc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(getRootPath());
